9c53a6cb7811f289ceab7f27a8ff47f0cfb5c540
matheustomczak-dev <matheus_tomczak@estudante.sesisenai.org.br>
2026-06-01 12:39:59 -0300

    doc - documentação do table

--- public/components/table.php
+++ public/components/table.php
@@ -1,5 +1,7 @@
 <h4>Usuários Cadastrados</h4>
 
+
+// aqui vamos criar uma tabela para mostrar os usuários cadastrados no banco de dados
 <table border="1" cellpadding="3">
 
     <tr>
@@ -7,17 +9,16 @@
         <th>Usuário</th>
         <th>Senha</th>
     </tr>
-
+    // aqui vamos usar o PHP para buscar os usuários cadastrados no banco de dados e mostrar na tabela
     <?php
-    
+    // aqui vamos criar a conexão com o banco de dados
     $sqlTodosUsuarios = "SELECT * FROM usuarios";
 
     $resultadoTodosUsuarios = $conn->query($sqlTodosUsuarios);
 
     while($linha = $resultadoTodosUsuarios->fetch_assoc()){
 
-    // o fetch assoc
-
+    //mostrar os usuários cadastrados na tabela
         echo "  <tr>
                     <td>". $linha['id'] . "</td>
                     <td>". $linha['usuario'] . "</td>
